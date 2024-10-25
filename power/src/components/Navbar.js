1322f9088e192a33e3bf3f36f328659f03351c7a
nicolamaraschi <nicola.maraschi01@gmail.com>
2024-10-25 03:24:02 +0200

    navbar sistemata appena clicchi un link si chiude da tel

--- power/src/components/Navbar.js
+++ power/src/components/Navbar.js
@@ -1,36 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      {/* Nome dell'azienda con il font Oswald */}
-      <a className="navbar-brand" href="#">
+      {/* Nome dell'azienda come link */}
+      <Link className="navbar-brand" to="/" onClick={handleLinkClick}>
         POWER RACING
-      </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      </Link>
+      <button 
+        className="navbar-toggler" 
+        type="button" 
+        onClick={handleToggle} 
+        aria-controls="navbarNav" 
+        aria-expanded={isOpen} 
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
         <ul className="navbar-nav mx-auto">
           <li className="nav-item active">
-            <Link className="nav-link" to="/">🏠 Home <span className="sr-only">(current)</span></Link>
+            <Link className="nav-link" to="/" onClick={handleLinkClick}>
+              🏠 Home <span className="sr-only">(current)</span>
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/chisiamo">👥 Chi siamo</Link>
+            <Link className="nav-link" to="/chisiamo" onClick={handleLinkClick}>
+              👥 Chi siamo
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/servizi">💼 Servizi</Link>
+            <Link className="nav-link" to="/servizi" onClick={handleLinkClick}>
+              💼 Servizi
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/vetrina">🚗 Vetrina usato</Link>
+            <Link className="nav-link" to="/vetrina" onClick={handleLinkClick}>
+              🚗 Vetrina usato
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/link">🔗 Link</Link>
+            <Link className="nav-link" to="/link" onClick={handleLinkClick}>
+              🔗 Link
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/contatti">📞 Contatti</Link>
+            <Link className="nav-link" to="/contatti" onClick={handleLinkClick}>
+              📞 Contatti
+            </Link>
           </li>
         </ul>
       </div>
